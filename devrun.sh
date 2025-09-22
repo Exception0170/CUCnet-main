e2b4630e981f4b4b1a4bb77200153d31f332044e
Exception0170 <kirill17.rzd@icloud.com>
2025-09-22 12:26:41 +0300

    delete shell scripts

--- devrun.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cd /home/server/website
-source .venv/bin/activate
-python3 app.py
--- run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cd /home/server/website
-source /home/server/website/.venv/bin/activate
-sudo gunicorn -b 0.0.0.0:80 app:app
