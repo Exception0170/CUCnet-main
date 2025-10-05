b86e3741d95c818a600f54e2c8037937c0c82aee
Exception0170 <kirill17.rzd@icloud.com>
2025-10-05 18:00:50 +0300

    Better symbols

--- website/routers/dev.py
+++ website/routers/dev.py
@@ -3,28 +3,35 @@ from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse, FileResponse
 from website.getservice import check_multiple_services
 
-router = APIRouter()
+router = APIRouter(prefix="/dev", tags=["dev", "error"])
 templates = Jinja2Templates(directory="website/templates")
 
 
+@router.get("/example")
+async def example(request: Request):
+    return templates.TemplateResponse("example.html",{
+        "request": request,
+        "title": "Example",
+        "page_title": "sys://Example"
+    })
+
+
 # Dev/test routes for errors
-
-
-@router.get("/dev/error/unauth")
+@router.get("/error/unauth")
 async def error_unauth():
     raise HTTPException(status_code=401, detail="Unauthorized")
 
 
-@router.get("/dev/error/server")
+@router.get("/error/server")
 async def error_server():
     raise Exception("Example error")
 
 
-@router.get("/dev/error/forbidden")
+@router.get("/error/forbidden")
 async def error_forbidden():
     raise HTTPException(status_code=403, detail="Forbidden")
 
 
-@router.get("/dev/win95")
+@router.get("/win95")
 async def test_win95(request: Request):
     return templates.TemplateResponse("win95.html", {"request": request})
