58cf48c7ad9d963b276dd54e7c4e17e2c5043ba0
Exception0170 <kirill17.rzd@icloud.com>
2025-10-12 00:29:17 +0300

    fix for prod part 3

--- website/routers/__init__.py
+++ website/routers/__init__.py
@@ -2,13 +2,12 @@ from fastapi import APIRouter
 
 
 from website.routers.blog import blog_router
-from website.routers.dashboard import dashboard_router
+
 from website.routers.dev import dev_router
 from website.routers.webpages import web_router
 
 main_router = APIRouter()
 main_router.include_router(web_router)
 main_router.include_router(dev_router)
-main_router.include_router(dashboard_router)
 
 main_router.include_router(blog_router)
